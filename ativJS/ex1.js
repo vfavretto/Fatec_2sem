d98066c1b28c165900d45724d50659d621c62ed1
vfavretto <vf.favretto@gmail.com>
2024-02-29 00:04:45 -0300

    Aula 2

---
+++ ativJS/ex1.js
@@ -0,0 +1,11 @@
+function calcularAumento() {
+  let salario = parseFloat(document.getElementById("salario").value);
+  let percentual = parseFloat(document.getElementById("percentual").value);
+
+  let aumento = (salario * percentual) / 100;
+  let novoSalario = salario + aumento;
+
+  document.getElementById(
+    "resultado"
+  ).innerText = `Seu salário com aumento será R$ ${novoSalario}.`;
+}
